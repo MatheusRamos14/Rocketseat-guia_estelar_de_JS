1c3058c3b6d13ff8ba206fc58067a9ab7c7c4523
Matheus Ramos <matheusdasilvaramos10@gmail.com>
2021-02-10 19:13:07 -0300

    Segundo módulo

---
+++ primeiros_passos/4-comentarios.js
@@ -0,0 +1,12 @@
+// Aqui eu tenho comentários em linha
+
+/*
+
+    Aqui eu tenho um comentário multi linha
+
+    O código abaixo escreve alguma mensagem no devtools
+
+*/
+
+console.log('Bem vindos aos Starter') // Alguma coisa 
+console.log('não iria ignorar')
