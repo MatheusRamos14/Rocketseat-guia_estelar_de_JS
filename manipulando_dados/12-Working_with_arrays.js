d739f037adc49824c088d9c35dc2e11401929512
Matheus Ramos <matheusdasilvaramos10@gmail.com>
2021-02-16 17:08:28 -0300

    Sétimo módulo

---
+++ manipulando_dados/12-Working_with_arrays.js
@@ -0,0 +1,33 @@
+// Manipulando Arrays
+
+let techs = ['hmtl', 'css', 'js']
+
+// Adicionar um item no fim
+
+console.log(techs.push("nodejs"))
+
+// Adicionar no começo
+
+console.log(techs.unshift('SQL'))
+
+// Remover do fim
+
+console.log(techs.pop())
+
+// Remover do começo
+
+console.log(techs.shift())
+
+// Pegar somente alguns elementos do array
+
+console.log(techs.slice(1, 3))
+
+// Remover 1 ou mais itens em qualquer posição do array
+
+console.log(techs.splice(1, 1))
+
+// Encontrar a posição de um elemento no array
+
+console.log(techs.findexOf('css'))
+
+console.log(techs)
---
+++ manipulando_dados/5-Decimals.js
@@ -0,0 +1,6 @@
+// Manipulando Strings e Números
+
+// Transformar um número quebrado com 2 casas decimais e trocar pontos por vírgula
+
+let number = 123.1326371236
+console.log(number.toFixed(2).replace('.', ','))
